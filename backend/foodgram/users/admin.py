3b381232c270feb3734892c555404727320c14f5
opendoor <90768681+OpenedClosed@users.noreply.github.com>
2022-09-11 16:16:19 +0300

    Delete backend directory

--- backend/foodgram/users/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-
-from .models import CustomUser
-
-
-@admin.register(CustomUser)
-class CustomUserAdmin(admin.ModelAdmin):
-    list_filter = ('email', 'username', )

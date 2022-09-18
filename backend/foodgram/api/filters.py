47a55e2d79a769eacf8f2adf304836dcd6298014
OpenedClosed <opendoor200179@gmail.com>
2022-09-18 14:21:37 +0300

    docker

--- backend/foodgram/api/filters.py
+++ backend/foodgram/api/filters.py
@@ -21,13 +21,13 @@ class RecipeFilterSet(FilterSet):
         user = self.request.user
         if value and user.is_authenticated:
             return queryset.filter(favorite__user=user)
-        # return queryset.filter(~Q(favorite__user=user))
+        return queryset.filter(~Q(favorite__user=user))
 
     def get_is_in_shopping_cart(self, queryset, name, value):
         user = self.request.user
         if value and user.is_authenticated:
             return queryset.filter(shoppingcart__user=user)
-        # return queryset.filter(~Q(shoppingcart__user=user))
+        return queryset.filter(~Q(shoppingcart__user=user))
 
     class Meta:
         model = Recipe

5010d051eeccef3daa897b969bac53b48d5a635f
OpenedClosed <opendoor200179@gmail.com>
2022-09-19 17:52:42 +0300

    add data

---
+++ backend/foodgram/recipes/management/commands/bd_script.py
@@ -0,0 +1,24 @@
+import csv
+
+from django.conf import settings
+from django.core.management import BaseCommand
+
+from recipes.models import Ingredient
+
+
+class Command(BaseCommand):
+
+    def handle(self, *args, **options):
+        with open(
+            f"{settings.BASE_DIR}/recipes/data/ingredients.csv",
+            "r",
+            encoding="utf-8",
+        ) as file:
+            file_reader = csv.reader(file)
+            for row in file_reader:
+                name, measurement_unit = row
+                Ingredient.objects.get_or_create(
+                    name=name,
+                    measurement_unit=measurement_unit)
+            file.close()
+        self.stdout.write(self.style.SUCCESS('Ингредиенты добавлены'))

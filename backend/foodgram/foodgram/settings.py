8593b1df9c2a0f26924362b0c3ffbfbf9ad13977
OpenedClosed <opendoor200179@gmail.com>
2022-09-19 10:40:12 +0300

    debug=false

--- backend/foodgram/foodgram/settings.py
+++ backend/foodgram/foodgram/settings.py
@@ -9,7 +9,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 SECRET_KEY = os.getenv('SECRET_KEY', default=get_random_secret_key())
 
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
